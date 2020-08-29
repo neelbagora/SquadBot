1e821b148847823597c8de0d036a621a157d5da6
Neville Hiramanek <nhiraman@ucsc.edu>
2020-08-28 22:49:15 -0700

    moved guap commands to guap.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ com = discord.ext.commands
 secret_token = os.getenv("TOKEN")
 description = '''I am Squad Bot.'''
 bot = commands.Bot(command_prefix='?', description=description)
-cogs=['cogs.basic','cogs.admin','cogs.valorant']
+cogs=['cogs.basic','cogs.admin','cogs.guap']
 
 def is_me(m):
     return m.author == bot.user
@@ -42,95 +42,95 @@ async def on_message(msg):
     guap = 0
     if not is_me(msg):    
         if (msg.content.lower() == 'bet'):
-            await msg.channel.send('bet')
+            await msg.channel.send(msg.content)
 
         if (msg.content.lower() == 'word'):
-            await msg.channel.send('word')
+            await msg.channel.send(msg.content)
         
         if msg.content.lower() == 'f':
-            await msg.channel.send("f")
+            await msg.channel.send(msg.content)
 
-        if msg.content.lower() == 'gimme cash':
-            with open("guap.json", "r") as f:
-                data = json.load(f)
-                if str(msg.author.id) in data.keys():
-                    guap = data[str(msg.author.id)]['guap']
-                    guap += 10
-                print(data)
-            with open("guap.json", "w") as f:
-                if guap == 0:
-                    data.update({
-                        str(msg.author.id):{
-                            'guap': 10}
-                    })
-                else:
-                    data.update({
-                        str(msg.author.id):{
-                            'guap': guap}
-                    })
-                json.dump(data, f)
-            await msg.channel.send("Here is ten guap $10.")
+        # if msg.content.lower() == 'gimme cash':
+        #     with open("guap.json", "r") as f:
+        #         data = json.load(f)
+        #         if str(msg.author.id) in data.keys():
+        #             guap = data[str(msg.author.id)]['guap']
+        #             guap += 10
+        #         print(data)
+        #     with open("guap.json", "w") as f:
+        #         if guap == 0:
+        #             data.update({
+        #                 str(msg.author.id):{
+        #                     'guap': 10}
+        #             })
+        #         else:
+        #             data.update({
+        #                 str(msg.author.id):{
+        #                     'guap': guap}
+        #             })
+        #         json.dump(data, f)
+        #     await msg.channel.send("Here is ten guap $10.")
         
-        if msg.content.lower() == 'guap':
-            with open("guap.json", "r") as f:
-                data = json.load(f)
-                if str(msg.author.id) in data.keys():
-                    guap = data[str(msg.author.id)]['guap']
-                    await msg.channel.send("{}, you got {} guap!".format(msg.author.name, guap))
+        # if msg.content.lower() == 'guap':
+        #     with open("guap.json", "r") as f:
+        #         data = json.load(f)
+        #         if str(msg.author.id) in data.keys():
+        #             guap = data[str(msg.author.id)]['guap']
+        #             await msg.channel.send("{}, you got {} guap!".format(msg.author.name, guap))
 
     await bot.process_commands(msg)
 
-@bot.event
-async def on_voice_state_update(member, before, after):
-    if member != bot.user:
-        print(member)
-        print(before)
-        print(after)
+# @bot.event
+# async def on_voice_state_update(member, before, after):
+#     if member != bot.user:
+#         print(member)
+#         print(before)
+#         print(after)
 
-        if (after.channel != None and after.channel.name == 'Guap Generator'):
-            with open("guap.json", "r") as f:
-                data = json.load(f)
-                print()
-                print(data)
-            with open("guap.json", "w") as f:
-                if (str(member.id) in data.keys() and 'joined' in data[str(member.id)].keys()):
-                    data[str(member.id)]['joined'] = time.time()
-                else:
-                    data.update({
-                        str(member.id): {
-                            'joined': time.time(),
-                            'guap': 0
-                        }
-                    })
-                print()
-                print(data)
-                json.dump(data, f)
-        elif (before.channel != None and before.channel.name == 'Guap Generator'):
-            with open("guap.json", "r") as f:
-                data = json.load(f)
-                print()
-                print(data)
-                guap = 0
-                joined_time = 0
+#         if (after.channel != None and after.channel.name == 'Guap Generator'):
+#             with open("guap.json", "r") as f:
+#                 data = json.load(f)
+#                 print()
+#                 print(data)
+#             with open("guap.json", "w") as f:
+#                 if (str(member.id) in data.keys() and 'joined' in data[str(member.id)].keys()):
+#                     data[str(member.id)]['joined'] = time.time()
+#                 else:
+#                     data.update({
+#                         str(member.id): {
+#                             'joined': time.time(),
+#                             'guap': 0
+#                         }
+#                     })
+#                 print()
+#                 print(data)
+#                 json.dump(data, f)
+#         elif (before.channel != None and before.channel.name == 'Guap Generator'):
+#             with open("guap.json", "r") as f:
+#                 data = json.load(f)
+#                 print()
+#                 print(data)
+#                 guap = 0
+#                 joined_time = 0
 
-                if (str(member.id) in data.keys()):
-                    joined_time = data[str(member.id)]['joined']
-                    guap = data[str(member.id)]['guap']
-                diff = int(time.time() - joined_time) / 10
+#                 if (str(member.id) in data.keys()):
+#                     joined_time = data[str(member.id)]['joined']
+#                     guap = data[str(member.id)]['guap']
+#                 diff = int(time.time() - joined_time) / 10
 
-            with open("guap.json", "w") as f:                   
-                guap += diff * 2
-                if (str(member.id) in data.keys()):
-                    data[str(member.id)]['guap'] = guap
-                else:
-                    data.update({
-                        str(member.id): {
-                            'joined': joined_time,
-                            'guap': guap
-                        }
-                    })
-                print(data)
-                json.dump(data, f)
+#             with open("guap.json", "w") as f:                   
+#                 guap += diff * 2
+#                 if (str(member.id) in data.keys()):
+#                     data[str(member.id)]['guap'] = guap
+#                 else:
+#                     data.update({
+#                         str(member.id): {
+#                             'joined': joined_time,
+#                             'guap': guap
+#                         }
+#                     })
+#                 print(data)
+#                 json.dump(data, f)
 
 @bot.command()
 @commands.has_permissions(manage_guild=True)
